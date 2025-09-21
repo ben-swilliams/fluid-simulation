0b6e90fd1abe738ebe8d0a476fa304bc63dee6bf
Ben Williams <46504256+ben-swilliams@users.noreply.github.com>
2025-09-21 11:05:34 +0100

    feat(11): Gravity! (#17)
    * Gravity!
    
    * Neatening some stuff up
    
    * Renamed script appropriately
    
    ---------
    
    Co-authored-by: Ben Williams <benwilliams@8afb775d.st-andrews.ac.uk>

--- 2D SPH/Assets/Scripts/Particle.cs
+++ 2D SPH/Assets/Scripts/Particle.cs
@@ -1,31 +1,39 @@
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+using UnityEngine.Rendering.Universal.Internal;
 
-public class GPUInstancing2D : MonoBehaviour
+public class Particle : MonoBehaviour
 {
-    [Header("Instancing Settings")]
+    [Header("Shaders")]
     [SerializeField] Shader shader;
+    [SerializeField] ComputeShader computeShader;
+
+    [Header("Instancing Settings")]
     [SerializeField] Mesh mesh;
     [SerializeField] int instanceCount = 1000;
     [SerializeField] Vector2 spawnArea = new Vector2(10f, 10f);
     [SerializeField] float size = 1;
 
+    [Header("Simulation Settings")]
+    [SerializeField] Vector2 gravity = new Vector2(0, -9.8f);
 
+    static int threadGroupSize = 64;
     Material instanceMaterial;
     ComputeBuffer positionBuffer;
+    ComputeBuffer velocityBuffer;
     ComputeBuffer argsBuffer;
+    Bounds bounds = new Bounds(Vector3.zero, Vector3.one * 1000f);
     uint[] args = new uint[5] { 0, 0, 0, 0, 0 };
+    int kernel;
     
     void Start()
     {
-        instanceMaterial = new Material(shader); 
-        
-        GenerateInstanceData();
+        instanceMaterial = new Material(shader);
         
         SetupBuffers();
+        InitialiseVariables();
     }
 
-    Vector2[] GenerateInstanceData()
+    Vector2[] GeneratePositionData()
     {
         Vector2[] positions = new Vector2[instanceCount];
 
@@ -41,32 +49,70 @@ public class GPUInstancing2D : MonoBehaviour
 
         return positions;
     }
-    
+
+    Vector2[] GenerateVelocityData()
+    {
+        Vector2[] velocities = new Vector2[instanceCount];
+
+        for (int i = 0; i < instanceCount; i++)
+        {
+            velocities[i] = Vector2.zero;
+        }
+
+        return velocities;
+    }
+
     void SetupBuffers()
     {
-        Vector2[] positions = GenerateInstanceData();
+        Vector2[] positions = GeneratePositionData();
         positionBuffer = new ComputeBuffer(instanceCount, sizeof(float) * 2);
         positionBuffer.SetData(positions);
-        
+
+        Vector2[] velocities = GenerateVelocityData();
+        velocityBuffer = new ComputeBuffer(instanceCount, sizeof(float) * 2);
+        velocityBuffer.SetData(velocities);
+
+        InitialiseArgsBuffer();
+    }
+
+    void InitialiseArgsBuffer()
+    {
         argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
-        
+
         args[0] = (uint)mesh.GetIndexCount(0);
         args[1] = (uint)instanceCount;
         args[2] = (uint)mesh.GetIndexStart(0);
         args[3] = (uint)mesh.GetBaseVertex(0);
         argsBuffer.SetData(args);
-        
-        instanceMaterial.SetBuffer("Positions", positionBuffer);
+    }
+
+    void InitialiseVariables()
+    {
         instanceMaterial.SetFloat("size", size);
+        instanceMaterial.SetBuffer("Positions", positionBuffer);
+
+        kernel = computeShader.FindKernel("Gravity");
+
+        computeShader.SetVector("gravity", gravity);
+        computeShader.SetInt("instanceCount", instanceCount);
+        computeShader.SetInt("threadGroupSize", threadGroupSize);
+
+        computeShader.SetBuffer(kernel, "Positions", positionBuffer);
+        computeShader.SetBuffer(kernel, "Velocities", velocityBuffer);
     }
     
     void Update()
     {
+        computeShader.SetFloat("deltaTime", Time.deltaTime);
+
+        int threadGroups = Mathf.CeilToInt(instanceCount / (float)threadGroupSize);
+        computeShader.Dispatch(kernel, threadGroups, 1, 1);
+
         Graphics.DrawMeshInstancedIndirect(
             mesh, 
             0,
             instanceMaterial, 
-            new Bounds(Vector3.zero, Vector3.one * 1000f), 
+            bounds,
             argsBuffer
         );
     }
@@ -75,7 +121,9 @@ public class GPUInstancing2D : MonoBehaviour
     {
         if (positionBuffer != null)
             positionBuffer.Release();
+        if (velocityBuffer != null)
+            velocityBuffer.Release();
         if (argsBuffer != null)
-            argsBuffer.Release();
+                argsBuffer.Release();
     }
 }
