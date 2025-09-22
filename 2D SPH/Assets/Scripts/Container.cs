272d8e6cbcf4edd65215948023a3a37a013b0c48
Ben Williams <46504256+ben-swilliams@users.noreply.github.com>
2025-09-22 16:20:49 +0100

    chore(19): separation of draw/simulate (#20)
    * Separated scene into low and high particle count scenes
    
    * Separated into Simulation and PRenderer (Unity has built-in called Renderer
    
    * Renamed some stuff, added nice comments to separate properties
    
    * Scene metadata change
    
    ---------
    
    Co-authored-by: Ben Williams <benwilliams@8afb7d5c.st-andrews.ac.uk>

--- 2D SPH/Assets/Scripts/Container.cs
+++ 2D SPH/Assets/Scripts/Container.cs
@@ -3,8 +3,20 @@ using UnityEngine.UIElements;
 
 public class Container : MonoBehaviour
 {
-    public Vector2 size = new Vector2(5f, 5f);
+    /*
+    Inspector properties
+    */
+    [SerializeField] Vector2 boundary = new Vector2(5f, 5f);
+
+    /*
+    Private properties
+    */
     LineRenderer lr;
+
+    /*
+    Public getters
+    */
+    public Vector2 Boundary => boundary;
     void Setup()
     {
         lr = GetComponent<LineRenderer>();
@@ -32,9 +44,9 @@ public class Container : MonoBehaviour
 
     void SetPoints()
     {
-        lr.SetPosition(0, new Vector3(-size.x / 2f, -size.y / 2f));
-        lr.SetPosition(1, new Vector3(size.x / 2f, -size.y / 2f));
-        lr.SetPosition(2, new Vector3(size.x / 2f, size.y / 2f));
-        lr.SetPosition(3, new Vector3(-size.x / 2f, size.y / 2f));
+        lr.SetPosition(0, new Vector3(-boundary.x / 2f, -boundary.y / 2f));
+        lr.SetPosition(1, new Vector3(boundary.x / 2f, -boundary.y / 2f));
+        lr.SetPosition(2, new Vector3(boundary.x / 2f, boundary.y / 2f));
+        lr.SetPosition(3, new Vector3(-boundary.x / 2f, boundary.y / 2f));
     }
 }
---
+++ 2D SPH/Assets/Scripts/Draw.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class Draw : MonoBehaviour
+{
+    /*
+    Inspector properties
+    */
+    [Header("Shaders")]
+    [SerializeField] Shader shader;
+
+    [Header("Appearance Settings")]
+    [SerializeField] Mesh mesh;
+
+    /*
+    Private properties
+    */
+    Simulate sim;
+    Material instanceMaterial;
+    Bounds bounds;
+
+    uint[] args = new uint[5] { 0, 0, 0, 0, 0 };
+    ComputeBuffer argsBuffer;
+
+    void Start()
+    {
+        sim = GetComponentInParent<Simulate>();
+        instanceMaterial = new Material(shader);
+        bounds = new Bounds(Vector2.zero, Vector2.one * 1000f);
+        InitialiseArgsBuffer();
+        InitialiseVariables();
+    }
+
+    void Update()
+    {
+        Graphics.DrawMeshInstancedIndirect(
+            mesh,
+            0,
+            instanceMaterial,
+            bounds,
+            argsBuffer
+        );
+    }
+
+    void InitialiseArgsBuffer()
+    {
+        argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+
+        args[0] = (uint)mesh.GetIndexCount(0);
+        args[1] = (uint)sim.InstanceCount;
+        args[2] = (uint)mesh.GetIndexStart(0);
+        args[3] = (uint)mesh.GetBaseVertex(0);
+        argsBuffer.SetData(args);
+    }
+
+    void InitialiseVariables()
+    {
+        instanceMaterial.SetFloat("size", sim.Size);
+        instanceMaterial.SetBuffer("Positions", sim.positionBuffer);
+    }
+
+    void OnDestroy()
+    {
+        if (argsBuffer != null)
+        {
+            argsBuffer.Release();
+            argsBuffer = null;
+        }
+    }
+
+}
