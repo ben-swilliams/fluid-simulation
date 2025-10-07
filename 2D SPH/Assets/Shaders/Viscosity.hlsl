05205558e3ba0921189e1d18212831af95afaab1
Ben Williams <benwilliams@8afb8397.st-andrews.ac.uk>
2025-10-07 13:16:21 +0100

    Wrote kernel/HLSL func for viscosity

--- 2D SPH/Assets/Shaders/Viscosity.hlsl
+++ 2D SPH/Assets/Shaders/Viscosity.hlsl
@@ -1,5 +1,7 @@
 float viscosityKernelLapConstant;
 
+float viscosityMultiplier;
+
 float ViscosityKernelLap(float2 offset) {
     float r = length(offset);
 
@@ -7,4 +9,10 @@ float ViscosityKernelLap(float2 offset) {
         return 0;
 
     return viscosityKernelLapConstant * (smoothingRadius - r);
+}
+
+float2 CalculateViscosityForce(uint i) {
+    float2 vForce = float2(0, 0);
+
+    return viscosityMultiplier * vForce;
 }
