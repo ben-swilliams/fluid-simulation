9cea33c2940a9e94ee2d8c6493a4058d2694e41e
Ben Williams <46504256+ben-swilliams@users.noreply.github.com>
2025-10-09 16:51:28 +0100

    feat(91): particle re-ordering (#92)
    * Removed debug output
    
    * Removed variable n
    
    * Scattering done! Looks like it works
    
    ---------
    
    Co-authored-by: Ben Williams <benwilliams@Mac.lan>

--- 2D SPH/Assets/Shaders/Partition.hlsl
+++ 2D SPH/Assets/Shaders/Partition.hlsl
@@ -5,20 +5,16 @@ static const uint primeZ = 83492791;
 uint gridX;
 uint gridY;
 
-uint CalculateHash(int2 gridPos) {
+uint CalculateHash(float2 pos) {
+    float2 offsetPos = pos + containerSize / 2;
+    int2 gridPos = int2(floor(offsetPos.x / smoothingRadius), floor(offsetPos.y / smoothingRadius));
     uint total = gridPos.x * primeX + gridPos.y * primeY;
     
     return total % tableSize;
 }
 
 void IndexAndCount(uint i) {
-    float2 pos = Positions[i];
-    pos += containerSize / 2;
-
-    int2 gridPos = int2(floor(pos.x / smoothingRadius), floor(pos.y / smoothingRadius));
-    uint hash = CalculateHash(gridPos);
-
-    GridIndices[i] = hash;
+    uint hash = CalculateHash(Positions[i]);
     
     InterlockedAdd(CellCounts[hash], 1);
 }
