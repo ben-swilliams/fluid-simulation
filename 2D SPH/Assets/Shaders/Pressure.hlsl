36b2c265692fea1a3554ca34666b7a01b03b4e9b
Ben Williams <46504256+ben-swilliams@users.noreply.github.com>
2025-10-13 12:43:11 +0100

    fix(95): ring artifacts (#101)
    Co-authored-by: Ben Williams <benwilliams@8afb80bf.st-andrews.ac.uk>

--- 2D SPH/Assets/Shaders/Pressure.hlsl
+++ 2D SPH/Assets/Shaders/Pressure.hlsl
@@ -6,7 +6,7 @@ float restDensity;
 float2 PressureKernelGrad(float2 offset) {
     float r = length(offset);
     
-    if (r < 0.0001 || r > smoothingRadius)
+    if (r < 1e-6 || r > smoothingRadius)
         return float2(0, 0);
     
     float inner = smoothingRadius - r;
