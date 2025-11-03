d4480471b5e25a60bfe44ab4a7a5aecda28e1ab8
Ben Williams <benwilliams@8afbfc65.st-andrews.ac.uk>
2025-11-03 15:58:55 +0000

    Moved some functions around

--- 2D SPH/Assets/Shaders/Pressure.hlsl
+++ 2D SPH/Assets/Shaders/Pressure.hlsl
@@ -6,32 +6,3 @@ float3 CalculatePressureContribution(uint i, uint j) {
 
     return (pressureI + pressureJ) * CubicSplineGrad(offset);
 }
-
-float3 CalculateXSPHPressureForce(uint i) {
-    float3 pressureForce = float3(0, 0, 0);
-    float3 xsphCorrection = float3(0, 0, 0);
-
-    int3 gridPosI = GetGridPos(Positions[i]);
-
-    for (int x = -1; x < 2; x++) {
-        for (int y = -1; y < 2; y++) {
-            for (int z = -1; z < 2; z++) {
-                int3 gridPosJ = gridPosI + int3(x, y, z);
-                if (!IsInBounds(gridPosJ)) continue;
-
-                uint hash = CalculateHashFromGrid(gridPosJ);
-
-                uint startIndex = Offsets[hash];
-                uint endIndex = Offsets[hash + 1];
-
-                for (uint j = startIndex; j < endIndex; j++) {
-                    if (i == j) continue;
-                    pressureForce += CalculatePressureContribution(i, j);
-                    xsphCorrection += CalculateXSPHContribution(i, j);
-                }
-            }
-        }
-    }
-
-    return -particleMass * particleMass * pressureForce + xsphCorrection;
-}
