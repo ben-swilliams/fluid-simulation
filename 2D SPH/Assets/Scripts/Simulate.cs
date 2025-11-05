b244d5d017cdeb82a94f93642831266319a5c22f
Ben Williams <benwilliams@8afb7d4c.st-andrews.ac.uk>
2025-11-05 14:50:15 +0000

    Removed debug

--- 2D SPH/Assets/Scripts/Simulate.cs
+++ 2D SPH/Assets/Scripts/Simulate.cs
@@ -85,26 +85,6 @@ public class Simulate : MonoBehaviour
         if (started)
         {
             AdvanceFrame();
-            float[] pressures = new float[instanceCount];
-            float[] densities = new float[instanceCount * 2];
-            Vector3[] accs = new Vector3[instanceCount];
-            Vector3[] positions = new Vector3[instanceCount];
-            Vector3[] velocities = new Vector3[instanceCount];
-            shader.Pressures.GetData(pressures);
-            shader.Densities.GetData(densities);
-            shader.Accelerations.GetData(accs);
-            shader.PositionBuffer.GetData(positions);
-            shader.VelocityBuffer.GetData(velocities);
-
-            for (int i = 0; i < instanceCount; i++)
-            {
-                if (float.IsNaN(pressures[i])) Debug.Log(i + ": PRESSURE");
-                if (float.IsNaN(densities[i])) Debug.Log(i + ": DENSITY");
-                if (float.IsNaN(densities[instanceCount + i])) Debug.Log(i + ": NEAR DENSITY");
-                if (float.IsNaN(accs[i].x)) Debug.Log(i + ": ACCELERATIONS");
-                if (float.IsNaN(positions[i].x)) Debug.Log(i + ": POSITIONS");
-                if (float.IsNaN(velocities[i].x)) Debug.Log(i + ": VELOCITIES");
-            }
         }
 
         drawer.DrawFrame();
