999daef3379d2434241e33129fdf0384b9405f50
Ben Williams <benwilliams@8afb85e0.st-andrews.ac.uk>
2025-11-14 15:40:49 +0000

    Moved UIHandler script

---
+++ 2D SPH/Assets/Scripts/UIHandler.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIHandler : MonoBehaviour
+{
+    [SerializeField] Color selectedColour;
+    [SerializeField] Button[] tabButtons;
+    [SerializeField] GameObject[] settings;
+    [SerializeField] GameObject WCSPHSettings;
+    [SerializeField] GameObject IISPHSettings;
+
+    int selectedIndex = 0;
+
+    void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(tabButtons[0].gameObject);
+        SelectTab(0);
+        RegisterListeners();
+    }
+
+    void RegisterListeners()
+    {
+        for (int i = 0; i < tabButtons.Length; i++)
+        {
+            int index = i;
+            tabButtons[i].onClick.AddListener(() =>
+            {
+                SelectTab(index);
+            });
+        }
+    }
+
+    void SelectTab(int index)
+    {
+        if (selectedIndex == index) return;
+
+        settings[selectedIndex].SetActive(false);
+        selectedIndex = index;
+        settings[selectedIndex].SetActive(true);
+    }
+
+    public void SelectSettings(int solver)
+    {
+        if (selectedIndex != 2) return;
+
+        if (solver == 0)
+        {
+            WCSPHSettings.SetActive(true);
+            IISPHSettings.SetActive(false);
+        }
+        if (solver == 1)
+        {
+            IISPHSettings.SetActive(true);
+            WCSPHSettings.SetActive(false);
+        }
+    }
+}
