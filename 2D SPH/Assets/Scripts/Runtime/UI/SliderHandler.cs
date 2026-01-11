62a26a36e2ca9c8cb1fbe132bf9124db3cd26add
Ben Williams <benwilliams@Bens-MacBook-Air.local>
2026-01-11 08:07:35 +0000

    Added slider labels to Scene

--- 2D SPH/Assets/Scripts/Runtime/UI/SliderHandler.cs
+++ 2D SPH/Assets/Scripts/Runtime/UI/SliderHandler.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,13 +6,15 @@ public class SliderHandler : MonoBehaviour
 {
     [SerializeField] Tweakable targetScript;
     [SerializeField] string targetProperty;
+    [SerializeField] TMP_Text label;
     
     Slider sourceSlider;
 
-    void Start()
+    void Awake()
     {
         sourceSlider = GetComponent<Slider>();
     }
+
     void Update()
     {
         sourceSlider.value = targetScript.Get(targetProperty);
@@ -20,5 +23,22 @@ public class SliderHandler : MonoBehaviour
     public void ChangeSetting(float x)
     {
        targetScript.Set(targetProperty, x); 
+    }
+
+    void OnEnable()
+    {
+        UpdateLabel(sourceSlider.value);
+        sourceSlider.onValueChanged.AddListener(UpdateLabel);
+    }
+
+    void OnDisable()
+    {
+        sourceSlider.onValueChanged.RemoveListener(UpdateLabel);
+    }
+
+    void UpdateLabel(float value)
+    {
+        if (!label) return;
+        label.text = value.ToString();
     }
 }
